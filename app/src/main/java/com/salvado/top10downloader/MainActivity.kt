da664a812b1bc6d4df1af52445fb8824d8dac2bf
salvado <luismiguelsalvado@gmail.com>
2021-06-26 16:39:19 +0100

    Initial commit

---
+++ app/src/main/java/com/salvado/top10downloader/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.salvado.top10downloader
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called!")
+        val downloadData = DownloadData()
+        downloadData.execute("URL goes here")
+        Log.d(TAG, "onCreate: done!")
+
+    }
+
+    companion object {
+        private class DownloadData: AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+
+            override fun doInBackground(vararg params: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${params[0]}")
+                return "doInBackground completed!"
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute: parameter is $result")
+            }
+        }
+    }
+}
